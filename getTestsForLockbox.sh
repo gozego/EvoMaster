0849a0017a713f0e3f4621e6d815719bd53fbf20
Robert Carrier <rcarrier@gozego.com>
2023-01-19 15:16:16 -0600

    Update lockbox to use testng

--- getTestsForLockbox.sh
+++ getTestsForLockbox.sh
@@ -7,4 +7,4 @@ source .env
 token="bearer $LOCKBOX_TOKEN"
 
 # shellcheck disable=SC2140
-java -jar core/target/evomaster.jar  --blackBox true --bbSwaggerUrl "$LOCKBOX_SWAGGER_URL"  --outputFolder "src/lockbox" --header0 "Authorization: $token" --outputFormat JAVA_JUNIT_4 --maxTime 30s --ratePerMinute 60
+java -jar core/target/evomaster.jar  --blackBox true --bbSwaggerUrl "$LOCKBOX_SWAGGER_URL"  --outputFolder "src/lockbox" --header0 "Authorization: $token" --outputFormat JAVA_TESTNG --maxTime 30s --ratePerMinute 60
